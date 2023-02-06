70a000193eb570eca89a54e1aa2e6c74d00762d5
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-02-06 17:08:02 +0100

    WIP

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = 1000
 
 app.use(express.static('public'))
 
 app.get('/', (request, response, next) => {
 
-    response.send("WELCOME TO MY HOMEPAGE")
+    response.sendFile(__dirname + '/views/home.html')
 })
 
 app.get('/About', (request, response, next) => {
 
-    response.send("the person")
+    response.sendFile(__dirname + '/views/about.html')
 })
 
 app.get('/Works', (request, response, next) => {
 
-    response.send("the work")
+    response.sendFile(__dirname + '/views/works.html')
+})
+
+app.get('/photogallery', (request, response, next) => {
+
+    response.sendFile(__dirname + '/views/photo.html')
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
 })
